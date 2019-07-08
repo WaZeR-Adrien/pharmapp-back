01d5f017254f800566745b6d116a5b0f0fe30957
Adrien <adrienm.martineau@gmail.com>
2019-07-08 09:43:57 +0200

    Conf for Heroku

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    database: {
+        dbname: 'pharmapp',
+        host: 'nas.adrien-martineau.fr',
+        port: 4407,
+        user: 'pharmapp',
+        password: 'pharm*app'
+    }
+}
+
+module.exports = config;
